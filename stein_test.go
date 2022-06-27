64c5bdf2fcd7ccbf5a998f0b92dfe7202dcd2814
Muhammad Ikhsan <muhammad.ikhsan@efishery.com>
2022-06-27 21:24:13 +0800

    feat: implement add

--- stein_test.go
+++ stein_test.go
@@ -88,3 +88,59 @@ func TestSearchParams_queryString(t *testing.T) {
 		assert.Equal(t, `offset=20&limit=10&search=%7B%22column_1%22%3A%22value_column_1%22%2C%22column_2%22%3A%22value_column_2%22%7D`, queryString)
 	})
 }
+
+func Test_stein_Add(t *testing.T) {
+	t.Run("should return the correct response", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			jsonBody := `{"updatedRange": "Sheet1!A3:B3"}`
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			_, _ = w.Write([]byte(jsonBody))
+		}))
+		defer ts.Close()
+
+		sc := New(ts.URL, nil)
+		resp, err := sc.Add("Sheet1", map[string]interface{}{
+			"column_1": "value_column_1",
+			"column_2": "value_column_2",
+		})
+		if err != nil {
+			t.Errorf("Error: %v", err)
+		}
+
+		assert.Equal(t, "Sheet1!A3:B3", resp.UpdatedRange)
+	})
+
+	t.Run("should return error if http code not 2xx", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusForbidden)
+		}))
+		defer ts.Close()
+
+		sc := New(ts.URL, nil)
+		_, err := sc.Add("sheetname", map[string]interface{}{})
+		assert.NotNil(t, err)
+		if !errors.As(err, &ErrNot2XX{}) {
+			t.Errorf("Expected ErrNot2XX, got %v", err)
+		}
+	})
+
+	t.Run("should return error on failed decode the response", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			jsonBody := `bad json`
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			_, _ = w.Write([]byte(jsonBody))
+		}))
+		defer ts.Close()
+
+		sc := New(ts.URL, nil)
+		_, err := sc.Add("/sheetname", map[string]interface{}{})
+		assert.NotNil(t, err)
+		if !errors.As(err, &ErrDecodeJSON{}) {
+			t.Errorf("Expected ErrDecode, got %v", err)
+		}
+	})
+}
